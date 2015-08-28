75a47bce13e591d82659815fa1b256602310bdf4
wawa0210 <xiaozhang0210@hotmail.com>
2015-08-28 14:43:45 +0800

    广播好使之后的第一次提交 ok 2015-08-28 14:43

--- BaiduPush/SendMessage/BrandCast.aspx.cs
+++ BaiduPush/SendMessage/BrandCast.aspx.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.UI;
-using System.Web.UI.WebControls;
 using BaseHelper;
 using ComBCL;
 using ComBCL.BdPush;
@@ -11,7 +7,7 @@ using Newtonsoft.Json;
 
 namespace BaiduPush.SendMessage
 {
-    public partial class BrandCast : System.Web.UI.Page
+    public partial class BrandCast : Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
@@ -25,7 +21,7 @@ namespace BaiduPush.SendMessage
 
             Notice_Android_Mod dam = new Notice_Android_Mod(txtTitle.Value,txtContent.Value);
             string json = JsonConvert.SerializeObject(dam);
-            Push_All_Mod pam = new Push_All_Mod(apikKey, json, (int)Baidu_Helper.Message_Type.Notice);
+            Push_All_Mod pam = new Push_All_Mod(apikKey, json, (int)Baidu_Helper.Message_Type.Notice,1);
             Push_All pa = new Push_All(secretKey, pam);
 
             txtResponse.Value= pa.PushMessage();
--- ComBCL/Push_All_Mod.cs
+++ ComBCL/Push_All_Mod.cs
@@ -10,11 +10,6 @@
 //
 //------------------------------------------------------------------------------------------------
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using ComBCL.BdPush;
 
 namespace ComBCL
@@ -23,32 +18,71 @@ namespace ComBCL
     {
 
         #region 属性
-        public uint msg_type { get; set; }      //number 	否 	取值如下：0：消息；1：通知。默认为0 	消息类型
-        public string msg { get; set; }         //string 	是 	详情见消息/通知数据格式 	消息内容，json格式
-        public uint msg_expires { get; set; }   //number 	否 	0~604800(86400*7)，默认为5小时(18000秒) 	相对于当前时间的消息过期时间，单位为秒
-        public uint deploy_status { get; set; } //number 	否 	取值为：1：开发状态；2：生产状态； 若不指定，则默认设置为生产状态。 	设置iOS应用的部署状态，仅iOS应用推送时使用
-        public uint send_time { get; set; }     //number    否  指定的实际发送时间，必须在当前时间60s以外，1年以内 	定时推送，用于指定的实际发送时间
+        /// <summary>
+        ///  number 	否 	取值如下：0：消息；1：通知。默认为0 	消息类型
+        /// </summary>
+        public uint msg_type
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// string 	是 	详情见消息/通知数据格式 	消息内容，json格式
+        /// </summary>
+        public string msg
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// number 	否 	0~604800(86400*7)，默认为5小时(18000秒) 	相对于当前时间的消息过期时间，单位为秒
+        /// </summary>
+        public uint msg_expires
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// number 	否 	取值为：1：开发状态；2：生产状态； 若不指定，则默认设置为生产状态。 	设置iOS应用的部署状态，仅iOS应用推送时使用
+        /// </summary>
+        public uint deploy_status
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// number    否  指定的实际发送时间，必须在当前时间60s以外，1年以内 	定时推送，用于指定的实际发送时间
+        /// </summary>
+        public uint send_time
+        {
+            get;
+            set;
+        }
         #endregion
 
         #region 构造函数
-        public Push_All_Mod(string apikey, string msg)
+        public Push_All_Mod(string apikey, string msg, uint msgExpires)
         {
             this.apikey = apikey;
             this.msg = msg;
             this.timestamp = Tool.getDefauleTimestamp();   //默认使用当前时间戳
             this.msg_type = 0;              //消息
-            this.msg_expires = 1;      //7天过期
+            this.msg_expires = msgExpires;      //过期时间(一秒为单位)
             this.device_type = 3;           //安卓
             this.deploy_status = 2;         //生产状态
         }
 
-        public Push_All_Mod(string apikey, string msg, uint msg_type)
+        public Push_All_Mod(string apikey, string msg, uint msgType, uint msgExpires)
         {
             this.apikey = apikey;
             this.msg = msg;
             this.timestamp = Tool.getDefauleTimestamp();   //默认使用当前时间戳
-            this.msg_type = msg_type;       //消息类型
-            this.msg_expires = 1;      //7天过期
+            this.msg_type = msgType;       //消息类型
+            this.msg_expires = msgExpires;      //过期时间(一秒为单位)
             this.device_type = 3;           //安卓
             this.deploy_status = 2;         //生产状态
         }
